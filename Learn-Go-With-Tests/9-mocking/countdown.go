fd972e4f8bf6e11c7589e18d23f9fa8097d6d155
jpgsaraceni <jpgomes@id.uff.br>
2021-12-01 21:41:29 -0300

    feat: Finish 'Mocking'

---
+++ Learn-Go-With-Tests/9-mocking/countdown.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+const finalWord = "Go!"
+const countdownStart = 3
+
+type Sleeper interface {
+	Sleep()
+}
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(500 * time.Millisecond)
+}
+
+func Countdown(out io.Writer, s Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		s.Sleep()
+		fmt.Fprintln(out, i)
+	}
+	s.Sleep()
+	fmt.Fprintln(out, finalWord)
+}
+
+func main() {
+	sleeper := DefaultSleeper{}
+	Countdown(os.Stdout, &sleeper)
+}
